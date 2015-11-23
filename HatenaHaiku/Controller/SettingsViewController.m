42973b8790b8f0a1b32c3d1381912495454d4eff
koogawa <koogawa.app@gmail.com>
2015-11-23 19:55:29 +0900

    Update footer

--- HatenaHaiku/Controller/SettingsViewController.m
+++ HatenaHaiku/Controller/SettingsViewController.m
@@ -9,6 +9,7 @@
 #import "SettingsViewController.h"
 #import "SettingsCountViewController.h"
 #import "KeywordViewController.h"
+#import "WebViewController.h"
 #import "AuthManager.h"
 
 @interface SettingsViewController ()
@@ -48,7 +49,7 @@
 	label.font = [UIFont systemFontOfSize:14];
 	label.adjustsFontSizeToFitWidth = YES;
 	label.numberOfLines = 1;
-	label.text = @"(c) 2013 koogawa";
+	label.text = @"(c) 2013 - 2015 @koogawa";
 	[tableFooterView addSubview:label];
     self.tableView.tableFooterView = tableFooterView;
 }
